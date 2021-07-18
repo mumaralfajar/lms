67407f8fef111019b1b4e3c76a57e71e41170b36
mumaralfajar <kuhaaku@gmail.com>
2021-07-18 21:37:49 +0700

    fix banyak

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -16,74 +16,26 @@ class CategoryTableSeeder extends Seeder
 
         if (!$is_exist->count()) {
             $category = new Category();
-            $category->name = 'Development';
-            $category->slug = 'development';
-            $category->icon_class = 'fa-chart-line';
+            $category->name = 'SD';
+            $category->slug = 'sd';
+            $category->icon_class = 'fa-school';
             $category->is_active = 1;
             $category->save();
 
             $category = new Category();
-            $category->name = 'Business';
-            $category->slug = 'business';
-            $category->icon_class = 'fa-business-time';
-            $category->is_active = 1;
-            $category->save();
-
-            $category = new Category();
-            $category->name = 'IT & Software';
-            $category->slug = 'IT-software';
-            $category->icon_class = 'fa-laptop';
-            $category->is_active = 1;
-            $category->save();
-
-            $category = new Category();
-            $category->name = 'Marketing';
-            $category->slug = 'marketing';
-            $category->icon_class = 'fa-funnel-dollar';
-            $category->is_active = 1;
-            $category->save();
-
-            $category = new Category();
-            $category->name = 'Lifestyle';
-            $category->slug = 'lifestyle';
-            $category->icon_class = 'fa-heartbeat';
-            $category->is_active = 1;
-            $category->save();
-
-            $category = new Category();
-            $category->name = 'Photography';
-            $category->slug = 'photography';
-            $category->icon_class = 'fa-camera-retro';
-            $category->is_active = 1;
-            $category->save();
-
-            $category = new Category();
-            $category->name = 'Health & Fitness';
-            $category->slug = 'health-fitness';
-            $category->icon_class = 'fa-medkit';
-            $category->is_active = 1;
-            $category->save();
-
-            $category = new Category();
-            $category->name = 'Teacher Training';
-            $category->slug = 'teacher-training';
+            $category->name = 'SMP';
+            $category->slug = 'smp';
             $category->icon_class = 'fa-chalkboard-teacher';
             $category->is_active = 1;
             $category->save();
 
             $category = new Category();
-            $category->name = 'Music';
-            $category->slug = 'music';
-            $category->icon_class = 'fa-music';
-            $category->is_active = 1;
-            $category->save();
-
-            $category = new Category();
-            $category->name = 'Academics';
-            $category->slug = 'academics';
+            $category->name = 'SMA';
+            $category->slug = 'sma';
             $category->icon_class = 'fa-user-graduate';
             $category->is_active = 1;
             $category->save();
+
         }
     }
 }
